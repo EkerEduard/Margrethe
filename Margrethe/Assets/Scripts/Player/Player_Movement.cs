a43faf7d78b7700d2f909f2b17c465c4fd4a6c01
EduardEker <ekermikro@gmail.com>
2024-06-10 16:25:27 +0200

    Character_Player #2
    Базовая настройка передвижения и гравитации для игрока. На данный момент я оставил видимой переменную gravtyScale так как возможно буду делать эксперименты с прыжком. (однако не знаю обоснованно ли это для игры )

--- Margrethe/Assets/Scripts/Player/Player_Movement.cs
+++ Margrethe/Assets/Scripts/Player/Player_Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,18 @@ using UnityEngine.InputSystem;
 public class Player_Movement : MonoBehaviour
 {
     private PlayerControlls controls;
+    private CharacterController characterController;
 
-    public Vector2 moveInput;
-    public Vector2 aimInput;
+    [Header("Movement info")]
+    [SerializeField] private float walkSpeed;
+    private Vector3 movementDirection;
+
+    private float verticalVelocity;
+    [SerializeField] private float gravityScale = 9.81f;
+
+
+    private Vector2 moveInput;
+    private Vector2 aimInput;
 
     private void Awake()
     {
@@ -19,6 +29,40 @@ public class Player_Movement : MonoBehaviour
 
         controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
         controls.Character.Aim.canceled += controls => aimInput = Vector2.zero;
+    }
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
+        ApplyGravity();
+
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * Time.deltaTime * walkSpeed);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded == false)
+        {
+            verticalVelocity -= gravityScale * Time.deltaTime;
+            movementDirection.y = verticalVelocity;
+        }
+        else
+        {
+            verticalVelocity = -0.5f;
+        }
     }
 
     // Acitve
