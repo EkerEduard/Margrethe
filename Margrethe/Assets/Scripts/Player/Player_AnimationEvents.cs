2b247b5eb5f660db443489b68c60ecb34c0e8af5
EduardEker <ekermikro@gmail.com>
2024-06-13 16:58:51 +0200

    Player_Weapon #5
    1)Прежде всего для улучшения работы в проекте я перенес компонент Player_WeaponVisuals на объект Player.
    Настроил синхронизацию слоев наследуем от Common weapon Layer
    2) Добавил анимации перезарядки для оружия(конечно не все так как хочется, у меня нет так много анимации, работаем с тем что есть).
    Сейчас в Player_WeaponVisuals много тестового кода, позже будет переработан в рабочий.
    3) Так же добавил анимации захвата оружия(1 - из-за спины 2 - сбоку). Пока я не забыл я нашел классную анимацию для поднятия оружия(equip).
    Настроил события для захвата и немного укоротил анимации. Поработал и с LeftHandIK чтоб захват и перезарядка смотрелись достойно.

---
+++ Margrethe/Assets/Scripts/Player/Player_AnimationEvents.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_AnimationEvents : MonoBehaviour
+{
+    private Player_WeaponVisuals visualController;
+
+    private void Start()
+    {
+        visualController = GetComponentInParent<Player_WeaponVisuals>();
+    }
+
+    public void ReloadIsOver()
+    {
+        visualController.MaximazeRigWeight();
+    }
+
+    public void ReturnRig()
+    {
+        visualController.MaximazeRigWeight();
+        visualController.MaximazeLeftHandWeight();
+    }
+
+
+    public void WeaponGrabIsOver()
+    {
+        visualController.SetBusyGrabbingWeaponTo(false);
+    }
+}
