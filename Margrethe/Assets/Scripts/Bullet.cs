054d06f61925a21c90775344550bdcf4b9bd8cf3
EduardEker <ekermikro@gmail.com>
2024-06-24 20:38:13 +0200

    Mini add-on #2
    Добавил из unity assets  набор эффектов, они очень хорошо подходят для оружия и еще нескольких вещей.
    Получилось сделать классный эффект bloom для моделей.
    Улучшил визуальную сторону пули. Сейчас хочу оставить себе заметку по поводу пули, точнее ее цвета и ее эффекта, в данный момент что-то рядом с зеленым, но думаю чуть позже подправить это, хотя мне нравится.

--- Margrethe/Assets/Scripts/Bullet.cs
+++ Margrethe/Assets/Scripts/Bullet.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletImpactFX;
+
     private Rigidbody rb => GetComponent<Rigidbody>();
 
     private void OnCollisionEnter(Collision collision)
     {
-        //rb.constraints = RigidbodyConstraints.FreezeAll;
+        CreateImpactFX(collision);
+
         Destroy(gameObject);
+    }
+
+    private void CreateImpactFX(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+
+        GameObject newImpactFX = Instantiate(bulletImpactFX, contact.point, Quaternion.LookRotation(contact.normal));
+
+        Destroy(newImpactFX, 1.0f); // ’мм... чуть позже попробовать 0.5
     }
 }
